3fbefe688bcd63a3f696c2dbf162fda76f201761
Vivek Dhole <94863770+vicky9168@users.noreply.github.com>
2026-06-10 21:00:23 +0530

    Update s3 bucket name

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-netflix-vivek-1" 
+    bucket = "devsecops-stackflow-vivek-1" 
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
